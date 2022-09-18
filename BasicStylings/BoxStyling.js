c1fa4d8609b82a52c51f796ccda0a98675188419
AsmaSimreen <asmasimreen1001@gmail.com>
2022-09-18 22:43:53 +0530

    Stylings in React

---
+++ BasicStylings/BoxStyling.js
@@ -0,0 +1,46 @@
+// Styling
+// 🐨 add a className prop to each div and apply the correct class names
+// based on the text content
+// 💰 Here are the available class names: box, box--large, box--medium, box--small
+// 💰 each of the elements should have the "box" className applied
+// 🐨 add a style prop to each div so their background color
+// matches what the text says it should be
+// 🐨 also use the style prop to make the font italic
+// 💰 Here are available style attributes: backgroundColor, fontStyle
+
+import * as React from 'react'
+import '../box-styles.css'
+
+
+const smallBox = (
+  <div className='box box--small'
+    style={{ fontStyle: 'italic', backgroundColor: 'lightblue' }}
+  >
+    small lightblue box
+  </div>
+)
+const mediumBox = (
+  <div
+    className='box box--medium'>
+    style={{ fontStyle: 'italic', backgroundColor: 'pink' }}
+    medium pink box
+  </div>
+)
+const largeBox = (
+  <div className='box box--large'>
+    style={{ fontStyle: 'italic', backgroundColor: 'orange' }}
+    large orange box
+  </div>
+)
+
+function App() {
+  return (
+    <div>
+      {smallBox}
+      {mediumBox}
+      {largeBox}
+    </div>
+  )
+}
+
+export default App
