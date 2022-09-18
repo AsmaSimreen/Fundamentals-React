86bfe13d5b331b6965302436d8d146ae9d2c8b12
AsmaSimreen <asmasimreen1001@gmail.com>
2022-09-18 23:07:01 +0530

    Extra Credit Solution

--- BasicStylings/BoxStyling.js
+++ BasicStylings/BoxStyling.js
@@ -11,36 +11,45 @@
 import * as React from 'react'
 import '../box-styles.css'
 
+function Box({className = '', ...otherProps}) {
+    return (
+    <div 
+    className={`box ${className}`} 
+    style={{fontStyle: 'italic', ...style}} 
+    {...otherProps}
+    />
+    )
+}
 
 const smallBox = (
-  <div className='box box--small'
-    style={{ fontStyle: 'italic', backgroundColor: 'lightblue' }}
-  >
-    small lightblue box
-  </div>
+    <Box className='box--small'
+        style={{ backgroundColor: 'lightblue' }}
+    >
+        small lightblue box
+    </Box>
 )
 const mediumBox = (
-  <div
-    className='box box--medium'>
-    style={{ fontStyle: 'italic', backgroundColor: 'pink' }}
-    medium pink box
-  </div>
+    <Box
+        className='box--medium'>
+        style={{ backgroundColor: 'pink' }}
+        medium pink box
+    </Box>
 )
 const largeBox = (
-  <div className='box box--large'>
-    style={{ fontStyle: 'italic', backgroundColor: 'orange' }}
-    large orange box
-  </div>
+    <Box className='box--large'>
+        style={{ backgroundColor: 'orange' }}
+        large orange box
+    </Box>
 )
 
 function App() {
-  return (
-    <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
-    </div>
-  )
+    return (
+        <div>
+            {smallBox}
+            {mediumBox}
+            {largeBox}
+        </div>
+    )
 }
 
 export default App
